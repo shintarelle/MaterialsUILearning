e84937dcde8dbe3fff724fb318372438e63fa082
AnnRozhkina <annrozhkina@gmail.com>
2024-05-09 10:58:27 +0300

    add stepper

---
+++ src/components/checkout/CheckoutStepper.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Step from '@mui/material/Step';
+import Button from '@mui/material/Button';
+import Stepper from '@mui/material/Stepper';
+import StepLabel from '@mui/material/StepLabel';
+import Typography from '@mui/material/Typography';
+
+const steps = ['Cart', 'Billing & address', 'Payment'];
+
+export default function HorizontalLinearStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: {
+            optional?: React.ReactNode;
+          } = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <>
+          <Typography sx={{ mt: 2, mb: 1 }}>All steps completed - you&apos;re finished</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{ mr: 1 }}>
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+}
--- src/sections/one/checkout.tsx
+++ src/sections/one/checkout.tsx
@@ -1,8 +1,22 @@
+'use client'
+
 import React from 'react'
 
+import { Container, Typography } from '@mui/material'
+
+import { useSettingsContext } from 'src/components/settings';
+import CheckoutStepper from 'src/components/checkout/CheckoutStepper';
+
 function Checkout() {
+  const settings = useSettingsContext();
   return (
-    <div>Checkout</div>
+    <Container maxWidth={settings.themeStretch ? false : 'xl'}>
+      <Typography variant="h4" sx={{ mb: 4 }}>
+        {' '}
+        Checkout{' '}
+      </Typography>
+      <CheckoutStepper />
+      </Container>
   )
 }
 
